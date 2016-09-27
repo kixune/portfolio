9c28c51c2f159fc4f7e329305f4ef1e64b4502ec
Barnabas Chan <barny@post.com>
2016-09-27 17:50:37 +0800

    add process.env.PORT for heroku to add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('./config/express'),
     app     = express();
 
-app.set('port', (1337));
+app.set('port', (process.env.PORT || 1337));
 
 app.listen(app.get('port'), function() {
   console.log('loading...\nrunninon localhost:1337')
