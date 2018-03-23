1b106a678848142b61e011839d18eeb2ddf7cc4a
Barnabas_Chan <Barny@CC-2s-MacBook-Pro.local>
2018-03-23 17:11:41 +0800

    [rename]

--- public/js/loader.js
+++ public/js/loader.js
@@ -4,7 +4,7 @@ console.log(loader);
 // Wait for window load
 $(function() {
 
-  // Hide the loader
+  	// Hide the loader
 	var loaded = $(".loader.page");
-  loaded.hide();
+  	loaded.hide();
 });
